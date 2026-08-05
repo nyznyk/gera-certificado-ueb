0ea26bd778174e1d71bcacfa6d0c95158bb4d06e
bea <84462580+nyznyk@users.noreply.github.com>
2026-08-05 09:15:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -190,7 +190,7 @@ function gerarCertificado() {
             
             // AJUSTES DAS ETAPAS (CONHECER, FAZER, COMPARTILHAR)
             const lineHeight = 30; 
-            const xInicioGeral = canvas.width * 0.19; 
+            const xInicioGeral = canvas.width * 0.12; 
             const limiteDireito = 0.91; 
             const maxWidthGeral = canvas.width * (limiteDireito - 0.19);
 
