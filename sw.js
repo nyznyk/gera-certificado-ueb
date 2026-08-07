e080c034ff71fcc0df3bcf03f3d082927688f191
bea <84462580+nyznyk@users.noreply.github.com>
2026-08-07 09:30:37 -0300

    Update CACHE_NAME to certificados-escoteiros-v5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'certificados-escoteiros-v4';
+const CACHE_NAME = 'certificados-escoteiros-v5';
 
 // Lista de todos os arquivos que o celular deve guardar na memória
 const ASSETS = [
